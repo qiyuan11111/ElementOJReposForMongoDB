490d479f9df4e541381d3260a34e33534c1b4cf7
qiyuan11111 <1746257500@qq.com>
2022-01-01 23:52:14 +0800

    修改目录结构

--- start.sh
+++ start.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-mongo << EOF
-use admin;
-db.auth("root", "ctguacm1234@");
-rs.initiate();
-use eloj;
-db.createUser(  
-    {  
-        user: "ctgu",  
-        pwd: "ctguacm1234@",  
-        roles: [  
-            { role: "readWrite", db: "eloj" }  
-        ]  
-    }  
- ); 
-db.createCollection("contest_enter");
-db.createCollection("contest_problem");
-EOF
+/usr/local/mongodb/bin/mongodb
+echo testetstestdtetatsda858558
 /bin/bash
