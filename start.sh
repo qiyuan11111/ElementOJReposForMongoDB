cdfc14c84379473884b14c5b52dc4b3c5e943f9d
qiyuan11111 <1746257500@qq.com>
2022-01-02 10:14:42 +0800

    修改目录结构

--- start.sh
+++ start.sh
@@ -1,4 +1,34 @@
 #!/bin/sh
-/usr/local/mongodb/bin/mongod --fork --logpath /usr/local/mongodb/mongodb.log --dbpath=/data/db
-echo testetstestdtetatsda858558
+/usr/local/mongodb/bin/mongod --fork --logpath /usr/local/mongodb/mongodb.log --dbpath /data/db
+/usr/local/mongodb/bin/mongo << EOF
+db.createUser({
+	user:"root", 
+	pwd:"ctguacm1234@",
+	roles:[{
+		role:"userAdminAnyDatabase",
+		db:"admin"
+	},{
+		role:"readWriteAnyDatabase",
+		db:"admin"
+	}]}
+);
+EOF
+/usr/local/mongodb/bin/mongod --shutdown --dbpath /data/db
+/usr/local/mongodb/bin/mongod --fork --logpath /usr/local/mongodb/mongodb.log --dbpath /data/db --auth --bind_ip 0.0.0.0
+/usr/local/mongodb/bin/mongo << EOF
+use admin;
+db.auth("root", "ctguacm1234@");
+rs.initiate();
+use eloj;
+db.createUser({  
+    user: "ctgu",  
+    pwd: "ctguacm1234@",  
+    roles: [{ 
+			role: "readWrite", 
+			db: "eloj" 
+	}]
+}); 
+db.createCollection("contest_enter");
+db.createCollection("contest_problem");
+EOF
 /bin/bash
